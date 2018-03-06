79936f54af8bac72d3ef70e0dbd03dc354796abf
Bro Qiang <broqiang@qq.com>
2018-03-06 20:15:55 +0800

    首次提交代码

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # laravel-markdown
 用来实现 markdown 富文本编辑器及预览
+
+首次提交，正在完成中……
---
+++ src/LaravelMarkdownProvider.php
@@ -0,0 +1,9 @@
+<?php 
+namespace BroQiang\LaravelMarkdown;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMarkdownProvider extends ServiceProvider
+{
+    
+}
